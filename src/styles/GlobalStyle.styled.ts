dcdfb5db0aae63fb535b1cb2f11a193e7ffb44f4
Matheus Mombach <mombachm@gmail.com>
2022-05-01 01:35:44 -0300

    code refactor

--- src/styles/GlobalStyle.styled.ts
+++ src/styles/GlobalStyle.styled.ts
@@ -1,21 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 import { fonts } from '../assets/fonts/fonts';
 
-export const MOBILE_MAX_SIZE = '600px';
+export const MOBILE_MAX_SIZE = '560px';
 
 const GlobalStyle = createGlobalStyle`
     ${fonts}
    *{
        margin: 0;
        padding: 0;
-       outline:0;
-       box-sizing:border-box;
+       outline: 0;
+       box-sizing: border-box;
        font-family: Work Sans, Rubik;
        color: #1E2A32;
        user-select: none;
    }
    #root{
-       margin:0 auto;
+       margin: 0 auto;
        height: 100vh;
    }
 `;
